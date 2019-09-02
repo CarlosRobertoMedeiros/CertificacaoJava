d5678cab9c4afd2c9bb085c3f85b7f5099efe9e8
Carlos Roberto Medeiros de Lima <carlosmedeiroslima@gmail.com>
2019-09-02 20:20:38 -0300

    Estudos para Certificação Pagina 50

---
+++ Variaveis/EscapeSequence/EscapeSequence.java
@@ -0,0 +1,15 @@
+public class EscapeSequence {
+	
+/*
+	Existem várias outras sequências de escape, sendo que as mais conhecidas são:
+	- \n para nova linha
+	- \\ para uma barra invertida
+	- \" para aspas duplas
+*/
+	public static void main(String []args){
+		System.out.println("Oi \"Maria\""); //Aspas duplas
+		System.out.println("Oi \n Maria"); //nova linha
+		System.out.println("Oi \\ Maria"); //Barra Invertida
+
+	}
+}
---
+++ Variaveis/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		
+		//Comentário Padrão
+		/*
+			Esse é um comentário em bloco
+		*/
+		System.out.println("Hello World !!!");
+
+	}
+
+}
